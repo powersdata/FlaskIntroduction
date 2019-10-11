d596c9891659a3873c5b354fb7293c7d138100b8
powersdata <ted@powersdata.com>
2019-10-11 15:01:47 -0400

    two in one main.py

--- main.py
+++ main.py
@@ -101,3 +101,57 @@ if __name__ == "__main__":
     # App Engine itself will serve those files as configured in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
     # app.run(debug=True)
+
+    
+    from datetime import datetime, timedelta
+from flask import Flask, render_template
+from google.cloud import storage
+project_id='gcp01-217016'
+bucket_name='stockanalysis01'
+client_storage = storage.Client(project_id)
+bucket = client_storage.get_bucket(bucket_name)
+
+def store_time(dt):
+    entity = datastore.Entity(key=datastore_client.key('visit'))
+    entity.update({
+        'timestamp': dt
+    })
+
+    datastore_client.put(entity)
+
+
+def fetch_times(limit):
+    query = datastore_client.query(kind='visit')
+    query.order = ['-timestamp']
+
+    times = query.fetch(limit=limit)
+
+    return times
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    # For the sake of example, use static information to inflate the template.
+    # This will be replaced with real information in later steps.
+    dummy_times = [datetime(2018, 1, 1, 10, 0, 0),
+                   datetime(2018, 1, 2, 10, 30, 0),
+                   datetime(2018, 1, 3, 11, 0, 0),
+                   datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                   ]
+
+    return render_template('index.html', times=dummy_times)
+
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [START gae_python37_render_template]
